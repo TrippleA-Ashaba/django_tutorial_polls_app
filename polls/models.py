07c998f8aef8a89f56b853cbf64590274c983982
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-12-08 10:42:45 +0300

    done according to the django docs

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -9,6 +10,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    @admin.display(boolean=True, ordering="pub-date", description="Published recently?")
     def was_published_recently(self):
         now = timezone.now()
 
